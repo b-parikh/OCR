73b0f5ec46891fc99fcd2d34bce53f1053204441
Ka Wong <kawong2020@u.northwestern.edu>
2018-12-13 18:02:16 -0600

    v2 in progress

--- Api/ml_lib/ocr.py
+++ Api/ml_lib/ocr.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.image as mpimg
+from matplotlib import pyplot as plt
 import cv2
 
 def process_img(img_filename):
@@ -8,66 +9,99 @@ def process_img(img_filename):
     new_shape[2] = 3
     X = X.reshape(new_shape)
 
-    X_gray = cv2.cvtColor(cv2.imread(img_filename).astype(np.uint8), cv2.COLOR_BGR2GRAY)
-    X_gray = X_gray.astype(np.uint8)
-    X_gray_smooth = cv2.GaussianBlur(X_gray, (9, 9), 0) # remove initial noise and smoothen lighting gradient
-    X_gray_smooth = cv2.adaptiveThreshold(X_gray_smooth, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-    X_gray_smooth = cv2.blur(X_gray_smooth, (3, 3))
-    X_gray_smooth = cv2.threshold(X_gray_smooth, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-    X_gray_smooth = cv2.medianBlur(X_gray_smooth, 13)
-    X_bw = 1 - np.round(X_gray_smooth/255)
-
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (9, 9))
-    X_bw =  cv2.dilate(X_bw, kernel, iterations = 3)
-
-    img = X_bw.astype(np.uint8)
+    X_processed = tune_img(X)
     _, labels = cv2.connectedComponents(img)
 
-    all_labels = []
-    all_locations = []
-    label_to_location = {}
-    pad_width = 3
-    
-    for i in range(1, len(np.unique(labels))):
-        l = labels.copy()
-        l[labels != i] = 0
-        
-        # Pad
-        l = np.hstack([np.zeros([l.shape[0], pad_width]), l]) #left
-        l = np.hstack([l, np.zeros([l.shape[0], pad_width])]) #right
-        l = np.vstack([np.zeros([pad_width, l.shape[1]]), l]) #up
-        l = np.vstack([l, np.zeros([pad_width, l.shape[1]])]) #down
-        
-        # Get dimensions of component
-        xs, ys = np.where(l != 0)
-        top = np.min(xs)
-        left = np.min(ys)
-        bottom = np.max(xs)
-        right = np.max(ys)
-    
-        # Square and resize
-        l_padded = l[top-pad_width:bottom+pad_width, left-pad_width:right+pad_width]
-        diff = abs(l_padded.shape[1] - l_padded.shape[0])
-        pad = diff // 2
-        if l_padded.shape[0] < l_padded.shape[1]:
-            l_square = np.vstack([np.zeros([pad, l_padded.shape[1]]), l_padded]) #up
-            l_square = np.vstack([l_square, np.zeros([pad + (diff % 2 == 1), l_padded.shape[1]])]) #down
-        elif l_padded.shape[0] >= l_padded.shape[1]:
-            l_square = np.hstack([np.zeros([l_padded.shape[0], pad]), l_padded]) #left
-            l_square = np.hstack([l_square, np.zeros([l_padded.shape[0], pad + (diff % 2 == 1)])]) #right
-        l_resized = cv2.resize(l_square, (45, 45))
-        l_resized[l_resized != 0] = 1
-        
-        # Erode based on size
-        all_locations.append(((top, left), (bottom, right)))
-        label_to_location[i] = {'tl':(top, left), 'br':(bottom, right)}
-        for dim in range(1, 12):
-            kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (dim, dim))
-            erosion = cv2.erode(l_resized, kernel, iterations = 1)
-            if np.sum(erosion)/np.sum(l_resized) < 0.4:
-                break
-        dim -= 1
+    components = get_components(labels)
+    return components
+
+
+
+
+def pad(arr, pad_width):
+    arr_new = np.hstack([np.zeros([arr.shape[0], pad_width]), arr]) #left
+    arr_new = np.hstack([arr_new, np.zeros([arr_new.shape[0], pad_width])]) #right
+    arr_new = np.vstack([np.zeros([pad_width, arr_new.shape[1]]), arr_new]) #up
+    arr_new = np.vstack([arr_new, np.zeros([pad_width, arr_new.shape[1]])]) #down
+    return arr_new
+
+def square(arr, pad_width, top, left, bottom, right):
+    arr_square = arr[top-pad_width:bottom+pad_width, left-pad_width:right+pad_width]
+    diff = abs(arr_square.shape[1] - arr_square.shape[0])
+    pad = diff // 2
+    if arr_square.shape[0] < arr_square.shape[1]:
+        arr_square = np.vstack([np.zeros([pad, arr_square.shape[1]]), arr_square]) #up
+        arr_square = np.vstack([arr_square, np.zeros([pad + (diff % 2 == 1), arr_square.shape[1]])]) #down
+    elif arr_square.shape[0] >= arr_square.shape[1]:
+        arr_square = np.hstack([np.zeros([arr_square.shape[0], pad]), arr_square]) #left
+        arr_square = np.hstack([arr_square, np.zeros([arr_square.shape[0], pad + (diff % 2 == 1)])]) #right
+    return arr_square
+
+def erode(arr, erosion_percent):
+    for dim in range(1, 12):
         kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (dim, dim))
-        erosion = cv2.erode(l_resized, kernel, iterations = 1)
-        all_labels.append(erosion)
-    return {'labels': all_labels, 'locations': label_to_location}
+        erosion = cv2.erode(arr, kernel, iterations = 1)
+        if np.sum(erosion)/np.sum(arr) < erosion_percent:
+            break
+    dim -= 1
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (dim, dim))
+    erosion = cv2.erode(arr, kernel, iterations = 1)
+    return erosion
+
+def get_components(labels, pad_width=3, erosion_percent=0.4):
+    components = {i:{'label':None, 
+                    'output':None, 
+                    'tl':None, 
+                    'br':None, 
+                    'pic':None,
+                    'group':None,
+                    'sup':False,
+                    'sub':False} 
+                 for i in range(1, len(np.unique(labels)))}
+
+    fig, axes = plt.subplots(2, int((len(components) + 1)/2), figsize=(15, 5))
+    for i, ax in zip(sorted(components.keys()), axes.ravel()):
+        label = labels.copy()
+        label[labels != i] = 0
+        label_padded = pad(label, pad_width)
+
+        # Get dimensions of component
+        xs, ys = np.where(label != 0)
+        top, bottom, left, right = np.min(xs), np.max(xs), np.min(ys), np.max(ys)
+        components[i]['tl'] = (top, left)
+        components[i]['br'] = (bottom, right)
+
+        # Square and resize
+        label_square = square(label_padded, pad_width, top, left, bottom, right)
+        label_square = cv2.resize(label_square, (45, 45))
+        label_square[label_square != 0] = 1
+
+        # Erode based on size
+        label_eroded = erode(label_square, erosion_percent)
+        components[i]['pic'] = label_eroded.ravel()
+    return components
+
+def tune_img(X, dilation_kernel=None, dilation_iterations=3):
+    # Binary
+    X_gray = cv2.cvtColor(cv2.imread(file_name_X).astype(np.uint8), cv2.COLOR_BGR2GRAY)
+
+    # Remove initial noise and smoothen lighting gradient
+    X_gray_smooth = cv2.GaussianBlur(X_gray, (11, 11), 0) #
+    # Threshold
+    X_gray_smooth = cv2.adaptiveThreshold(X_gray_smooth, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+
+    # Blur
+    X_gray_smooth = cv2.blur(X_gray_smooth, (3, 3))
+    # Threshold
+    X_gray_smooth = cv2.threshold(X_gray_smooth, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+    # Median blur
+    X_gray_smooth = cv2.medianBlur(X_gray_smooth, 17)
+    # Threshold
+    X_bw = 1 - np.round(X_gray_smooth/255)
+
+    # Dilate
+    if not dilation_kernel: dilation_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10, 10))
+    X_bw =  cv2.dilate(X_bw, dilation_kernel, iterations=dilation_iterations)
+
+    return X_bw.astype(np.uint8)
